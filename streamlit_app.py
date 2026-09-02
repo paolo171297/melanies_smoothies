a7488222a8746050266fcb54e9e706a9e87975e1
paolo171297 <dattolico_007@live.it>
2026-09-02 18:08:16 +0200

    Correct typos and update smoothie order logic
    Fix typos and improve code structure in Streamlit app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,46 +1,92 @@
 # Import python packages
+
 import streamlit as st
+
 from snowflake.snowpark.functions import col
+
 import requests  
 
+
+
+
+
 # Write directly to the app
+
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
-st.write("Choose the fruits you want in your custom Smoothie!")
+
+st.write("Choose the fruits you want in your custon Smoothie!"
+
+)
+
+
 
 name_on_order = st.text_input('Name on Smoothie:')
-st.write('The name on your Smoothie will be:', name_on_order)
+
+st.write('The name on your Smoothie wil be:', name_on_order)
+
+
 
 cnx = st.connection("snowflake")
+
 session = cnx.session()
 
-# Aggiunta la parentesi di chiusura ) in fondo
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON')
 
-# Convertiamo il dataframe Snowpark in un Dataframe Pandas per cercare facilmente SEARCH_ON
-pd_df = my_dataframe.to_pandas()
+st.dataframe(data=my_dataframe, use_container_width=True)
 
-ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
+st.stop()
+
+ingredients_list = st.multiselect( 'Choose up to 5 ingredients:', my_dataframe, max_selections=5)
+
+
 
 if ingredients_list:
+
+
+
+    
+
     ingredients_string = ''
 
+
+
     for fruit_chosen in ingredients_list:
+
         ingredients_string += fruit_chosen + ' '
-        
-        # Recupera il valore esatto della colonna SEARCH_ON corrispettivo al frutto selezionato
-        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        
-        st.subheader(fruit_chosen + ' Nutrition Information')
-        
-        # Usa l'URL corretto 'www.smoothiefroot.com' e passa la variabile 'search_on'
-        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/" + search_on)  
+
+        st.subheader(fruit_chosen + 'Nutrition Information')
+
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
+
+
+    #st.write(ingredients_string)
+
+
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                        values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+                    values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
+
+
+
+    #st.write(my_insert_stmt)
 
     time_to_insert = st.button('Submit Order') 
 
+
+
     if time_to_insert: 
+
         session.sql(my_insert_stmt).collect()
+
+        
+
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+
+
+
+
