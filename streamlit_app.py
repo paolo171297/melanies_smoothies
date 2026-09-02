9829484d72e358315ed05e65ca2a0f7313770354
paolo171297 <dattolico_007@live.it>
2026-09-02 18:28:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         
         # Usa search_on e l'URL corretto dell'API
-        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/" + search_on)  
+        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
